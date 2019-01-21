c84862f3d077f01531b0d5fb079f25248534484a
tonymbarton <36773585+tonymbarton@users.noreply.github.com>
2019-01-21 13:51:41 -0500

    Add files via upload

---
+++ Office365ForceSignOff.ps1
@@ -0,0 +1,5 @@
+﻿# Make sure you first install AzureAD module with Install-Module -Name AzureAD
+
+Connect-AzureAD -credential $credential
+
+Get-AzureADUser -all $true | where {$_.userprincipalname -like "*mydomain.com"} | Revoke-AzureADUserAllRefreshToken
